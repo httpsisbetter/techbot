9f0d980362e629174be35f3d5e95954749273ea5
httpsisbetter <119723641+httpsisbetter@users.noreply.github.com>
2023-08-29 18:04:28 -0700

    Create keys.py

---
+++ lib/data/keys.py
@@ -0,0 +1,12 @@
+TOKEN = 'TOKENHERE'
+
+
+OWNERS = [IDHERE, IDHERE, IDHERE, IDHERE]
+
+# Webhooks for Discord
+
+new_users = 'WEBHOOKHERE'
+expired_users = 'WEBHOOKHERE'
+bombing_logs = 'WEBHOOKHERE'
+bombing_requests = 'WEBHOOKHERE'
+failed_admin = 'WEBHOOKHERE'
